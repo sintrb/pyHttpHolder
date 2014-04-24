1db42190809e8726a6221058076be4403c60570e
RobinTang <sintrb@gmail.com>
2014-04-24 14:48:01 +0800

    添加使用例子

--- HttpHolder.py
+++ HttpHolder.py
@@ -141,5 +141,37 @@ class HttpHolder:
 		return dict(((c.name, c.value) for c in self.cj))
 
 
+if __name__ == '__main__':
+	import json
+	headers = {
+		# Chrome User-Agent
+		'User-Agent':'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36',
+		}
+	# 使用Chrome的User-Agent，设置默认超时为10s
+	h = HttpHolder(headers=headers, timeout=10)
+	
+	# 请求豆瓣首页并以便获取相关Cookie，该次请求使用GET方法
+	h.open_html('http://www.douban.com')
+	
+	
+	# 登陆表单
+	form = {
+		'email':'abc@def.com',
+		'password':'123',
+		'app_name':'radio_desktop_win',
+		'version':'100'
+		}
+	# 尝试登陆，该次请求使用POST方法，因为data有数据，返回JSON字串
+	res = h.open_html('http://www.douban.com/j/app/login', 
+					headers={'Referer':'http://www.douban.com'}, 
+					data=form)
+	# 解析JSON
+	reso = json.loads(res)
+	if 'err' in reso:
+		# 有错误，输出
+		print reso['err']
+
+	# 打印现在的Cookie
+	print h.get_cookiesdict()
 
 
